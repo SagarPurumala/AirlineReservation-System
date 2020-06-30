961b1fb4867bb98ee227d428c2aca3354585fb50
SagarPurumala <61617097+SagarPurumala@users.noreply.github.com>
2020-07-01 00:09:03 +0530

    commit

--- airlinereservationsystemhibernate/src/main/java/com/jfsfeb/airlinereservationsystemhibernate/dao/UserDAOJPAImpl.java
+++ airlinereservationsystemhibernate/src/main/java/com/jfsfeb/airlinereservationsystemhibernate/dao/UserDAOJPAImpl.java
@@ -2,22 +2,57 @@ package com.jfsfeb.airlinereservationsystemhibernate.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
 import com.jfsfeb.airlinereservationsystemhibernate.dto.BookingStatus;
 import com.jfsfeb.airlinereservationsystemhibernate.dto.FlightDetails;
 import com.jfsfeb.airlinereservationsystemhibernate.dto.User;
+import com.jfsfeb.airlinereservationsystemhibernate.exception.ARSException;
 
 public class UserDAOJPAImpl implements UserDAO{
-
+	EntityManagerFactory factory = null;
 	@Override
 	public boolean registerUser(User user) {
-		// TODO Auto-generated method stub
-		return false;
+		EntityManager manager = null;
+		EntityTransaction transaction = null;
+		try {
+			factory = Persistence.createEntityManagerFactory("TestPersistence");
+			manager = factory.createEntityManager();
+			transaction = manager.getTransaction();
+			transaction.begin();
+			manager.persist(user);
+			transaction.commit();
+			return true;
+		} catch (Exception e) {
+			transaction.rollback();
+			throw new ARSException("User Already Exists Or User Can't Be added");
+		} finally {
+			manager.close();
+			factory.close();
+		}
 	}
 
 	@Override
 	public User authenticateUser(String emailId, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager manager = null;
+		factory = Persistence.createEntityManagerFactory("TestPersistence");
+		manager = factory.createEntityManager();
+		String jpql = "select u from  User u where u.emailId = :emailId and u.password =:password";
+		TypedQuery<User> query = manager.createQuery(jpql, User.class);
+		query.setParameter("emailId", emailId);
+		query.setParameter("password", password);
+		try {
+			return query.getSingleResult();
+		} catch (Exception e) {
+			throw new ARSException("Invalid Login Credentials, Please Enter Correctly");
+		} finally {
+			manager.close();
+			factory.close();
+		}
 	}
 
 	@Override
@@ -40,8 +75,15 @@ public class UserDAOJPAImpl implements UserDAO{
 
 	@Override
 	public List<FlightDetails> getFlightDetails() {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager manager = null;
+		factory = Persistence.createEntityManagerFactory("TestPersistence");
+		manager = factory.createEntityManager();
+		String jpql = "select f from FlightDetails f";
+		TypedQuery<FlightDetails> query = manager.createQuery(jpql, FlightDetails.class);
+		List<FlightDetails> recordlist = query.getResultList();
+		manager.close();
+		factory.close();
+		return recordlist;
 	}
 
 	@Override
