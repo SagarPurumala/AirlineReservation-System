fd38dd33becf39c61959f64f0c7276c2b0799b97
SagarPurumala <61617097+SagarPurumala@users.noreply.github.com>
2020-07-04 20:33:25 +0530

    commit

--- airlinereservationsystemspring/src/main/java/com/jfsfeb/airlinereservationsystemspring/exception/ARSException.java
+++
@@ -1,8 +0,0 @@
-package com.jfsfeb.airlinereservationsystemspring.exception;
-@SuppressWarnings("serial")
-public class ARSException extends RuntimeException {
-	public ARSException(String msg) {
-		super(msg);
-	}
-
-}
